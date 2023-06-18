e47b96a1c08d3ec6f00a430d61754fb05341d123
Samuel Simões <samuelsimoes31@gmail.com>
2023-06-18 15:38:19 -0300

    feat: DropDown full controlled with react hook form

--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -1,31 +1,54 @@
-import { FieldPathByValue, useFormContext } from 'react-hook-form';
+import { Controller, ControllerProps, FieldPathByValue, useFormContext } from 'react-hook-form';
 import DropDown, { DropDownPropsInterface } from 'react-native-paper-dropdown';
 import { FormContext } from '../hooks/FormContext';
 import { useState } from 'react';
 import { storage } from '../clients/mmkv';
+import { View, ViewProps } from 'react-native';
+import { HelperText } from 'react-native-paper';
+import { getDeepVal } from '../utils/form';
 
-interface DropdownP extends Omit<DropDownPropsInterface, 'visible' | 'showDropDown' | 'onDismiss' | 'setValue' | 'value'> {
-  name: FieldPathByValue<FormContext, string>;
-  MMKVKey?: FieldPathByValue<FormContext, string> | string
-}
+type DropdownP =
+  Omit<ControllerProps<FormContext>, 'render' | 'name'> &
+  Omit<DropDownPropsInterface, 'visible' | 'showDropDown' | 'onDismiss' | 'setValue' | 'value'> & {
+    containerStyle?: ViewProps['style'];
+    name: FieldPathByValue<FormContext, string>;
+    MMKVKey?: FieldPathByValue<FormContext, string> | string;
+  };
 
-export default function Dropdown({ name, MMKVKey, ...props }: DropdownP) {
-  const { setValue, watch } = useFormContext<FormContext>();
+export default function Dropdown({ containerStyle, name, rules, defaultValue, shouldUnregister, MMKVKey, ...props }: DropdownP) {
+  const { control, formState } = useFormContext<FormContext>();
   const [showDropDown, setShowDropDown] = useState(false);
+  const error = getDeepVal(formState.errors, name);
   return (
-    <DropDown
-      {...props}
-      visible={showDropDown}
-      showDropDown={() => setShowDropDown(true)}
-      onDismiss={() => setShowDropDown(false)}
-      setValue={(value) => {
-        setValue(name, value);
-        if(MMKVKey){
-          storage.set(MMKVKey, value)
-        }
-      }}
-      value={watch(name)}
-      mode='outlined'
-    />
+    <View style={containerStyle}>
+      <Controller
+        name={name}
+        control={control}
+        rules={rules}
+        defaultValue={defaultValue}
+        shouldUnregister={shouldUnregister}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <DropDown
+            {...props}
+            visible={showDropDown}
+            showDropDown={() => setShowDropDown(true)}
+            onDismiss={() => {
+              onBlur()
+              setShowDropDown(false)
+            }}
+            setValue={(value) => {
+              onChange(value);
+              if (MMKVKey) {
+                storage.set(MMKVKey, value);
+              }
+            }}
+            value={value}
+            mode='outlined'
+          />
+        )}
+      />
+      <HelperText visible={!!error} type='error'>{error?.message}</HelperText>
+    </View>
+
   );
 }
