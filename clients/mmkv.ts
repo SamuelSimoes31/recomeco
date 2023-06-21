bd428037c52e647504ba7ce62d426bfab33d4bb9
Samuel Simões <samuelsimoes31@gmail.com>
2023-06-20 22:08:26 -0300

    feat: add flipper and mmkv plugin

--- clients/mmkv.ts
+++ clients/mmkv.ts
@@ -1,7 +1,13 @@
 import { FieldPathByValue } from 'react-hook-form';
 import { MMKV } from 'react-native-mmkv'
 import { FormContext } from '../hooks/FormContext';
+import { initializeMMKVFlipper } from "react-native-mmkv-flipper-plugin";
 
 export const storage = new MMKV()
 
 export const MMKVGetFormString = (key: FieldPathByValue<FormContext, string>) => storage.getString(key)
+
+// add this line inside your App.tsx
+if (__DEV__) {
+  initializeMMKVFlipper({ default: storage });
+}
