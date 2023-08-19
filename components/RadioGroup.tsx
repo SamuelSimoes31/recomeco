04a3be8a60067d2b9f9394fa31eac19c782f2f83
Samuel Simões <samuelsimoes31@gmail.com>
2023-08-19 19:14:21 -0300

    feat: Controlled RadioGroup

--- components/RadioGroup.tsx
+++ components/RadioGroup.tsx
@@ -1,27 +1,63 @@
-import React from 'react'
-import { useFormContext } from 'react-hook-form';
-import { View, Text } from 'react-native';
-import { RadioButton } from 'react-native-paper';
-import { FormContext } from '../app/_layout';
+import React from 'react';
+import { Controller, ControllerProps, FieldPath, FieldPathByValue, useFormContext } from 'react-hook-form';
+import { View, Text, ViewProps } from 'react-native';
+import { HelperText, RadioButton } from 'react-native-paper';
+import { FormContext } from '../hooks/FormContext';
+import { getDeepVal } from '../utils/form';
+import { storage } from '../clients/mmkv';
 
-interface RadioGroupP {
-  name: keyof FormContext,
-  label: string
-  options: string[]
-}
 
-export default function RadioGroup({name, label, options}: RadioGroupP) {
-  const { setValue, watch } = useFormContext<FormContext>();
-  const value = watch(name)
+type RadioGroupP = Omit<ControllerProps<FormContext>, 'render' | 'name' | 'control'> & {
+  name: FieldPathByValue<FormContext, string>,
+  label: string;
+  options: string[];
+  containerStyle?: ViewProps['style'];
+  MMKVKey?: FieldPathByValue<FormContext, string> | string;
+  required?: boolean;
+};
+
+export default function RadioGroup({
+  name,
+  label,
+  options,
+  rules,
+  required,
+  defaultValue,
+  shouldUnregister,
+  MMKVKey,
+}: RadioGroupP) {
+  const { control, formState } = useFormContext<FormContext>();
+  const error = getDeepVal(formState.errors, name);
   return (
-    <RadioButton.Group onValueChange={newValue => setValue(name, newValue)} value={typeof value === 'number' ? String(value) : value}>
-    <Text>{label}</Text>
-    {options.map(option => (
-      <View key={option} style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <RadioButton value={option} />
-        <Text>{option}</Text>
-      </View>
-    ))}
-  </RadioButton.Group>
-  )
+    <Controller
+      name={name}
+      control={control}
+        rules={{
+          ...rules as any,
+          required: required ? {value: true, message: 'Esse campo é obrigatório'} : rules?.required,
+        }}
+        defaultValue={defaultValue}
+        shouldUnregister={shouldUnregister}
+      render={({ field: { onChange, value } }) => (
+        <RadioButton.Group
+          onValueChange={newValue => {
+            onChange(newValue)
+            if (MMKVKey) {
+              storage.set(MMKVKey, newValue);
+            }
+          }}
+          value={value}
+        >
+          <Text>{label}</Text>
+          {options.map(option => (
+            <View key={option} style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <RadioButton value={option} />
+              <Text>{option}</Text>
+            </View>
+          ))}
+           <HelperText visible={!!error} type='error'>{error?.message}</HelperText>
+        </RadioButton.Group>
+      )}
+    />
+  );
 }
